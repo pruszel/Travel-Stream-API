3da1eec7cb584e1f04e24c3cc3c53c49515631dd
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-02-25 14:47:53 -0800

    Test GitHub Action for deploying to Fly.io

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -5,4 +5,4 @@ class HomeViewTests(TestCase):
     def test_home_view(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hello World')
+        self.assertContains(response, 'Hello, World!')
