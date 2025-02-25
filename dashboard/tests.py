8b3b2aac06cbed9f12fd0a69e71927bb3bf3d9a9
Peter Ruszel <3279147+pruszel@users.noreply.github.com>
2025-02-25 13:40:49 -0800

    Add simple dashboard home view with test

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomeViewTests(TestCase):
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Hello World')
